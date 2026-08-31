def0916d32b052c4168184d7a6832e10b12748e9
Gabriella Piccolo <g.p71802@gmail.com>
2026-08-30 20:34:44 -0400

    Add additional edge case tests for GradeAnalyzer

--- module2/GradeAnalyzerTest.java
+++ module2/GradeAnalyzerTest.java
@@ -35,4 +35,24 @@ public class GradeAnalyzerTest {
         ArrayList<Integer> scores = new ArrayList<>(Arrays.asList(88, 88, 88)); 
         assertEquals(88.0, GradeAnalyzer.calculateAverage(scores)); 
     }
+
+    @Test
+    void calculateAverage_withTenScores_returnsExactAverage() {
+    ArrayList<Integer> scores = new ArrayList<>();
+
+    scores.add(70);
+    scores.add(75);
+    scores.add(80);
+    scores.add(85);
+    scores.add(90);
+    scores.add(95);
+    scores.add(60);
+    scores.add(65);
+    scores.add(100);
+    scores.add(80);
+
+    double average = GradeAnalyzer.calculateAverage(scores);
+
+    assertEquals(80.0, average);
+    }
 }
